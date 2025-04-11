b06d093bc8de050de07312683abe4b8531e3795a
dinhvandong <dongvan.ict3@gmail.com>
2025-04-11 14:41:27 +0700

    update code backend

---
+++ backend/quanlytruonghoc-bootcamp/src/main/java/com/vti/quanlytruonghoc_bootcamp/QuanlytruonghocBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.vti.quanlytruonghoc_bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlytruonghocBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlytruonghocBootcampApplication.class, args);
+	}
+
+}
---
+++ backend/quanlytruonghoc-bootcamp/src/test/java/com/vti/quanlytruonghoc_bootcamp/QuanlytruonghocBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.quanlytruonghoc_bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlytruonghocBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
